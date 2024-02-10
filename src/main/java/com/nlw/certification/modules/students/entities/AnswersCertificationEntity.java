1651db6948da8a362f1a78a0c89a177207d8fd89
kevinfjq <kevifjq@gmail.com>
2024-02-10 15:07:40 -0300

    feat: add database mapping for students and certifications

--- src/main/java/com/nlw/certification/modules/students/entities/AnswersCertificationEntity.java
+++ src/main/java/com/nlw/certification/modules/students/entities/AnswersCertificationEntity.java
@@ -1,17 +1,44 @@
 package com.nlw.certification.modules.students.entities;
 
+import jakarta.persistence.*;
 import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity(name = "answers_certification_students")
 public class AnswersCertificationEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
+
+    @Column(name = "certification_id")
     private UUID certificationID;
+
+    @ManyToOne
+    @JoinColumn(name = "certification_id", insertable = false, updatable = false)
+    private CertificationStudentEntity certificationStudentEntity;
+
+    @Column(name = "student_id")
     private UUID studentID;
+
+    @ManyToOne
+    @JoinColumn(name = "student_id", insertable = false, updatable = false)
+    private StudentEntity studentEntity;
+
+    @Column(name = "question_id")
     private UUID questionID;
+
+    @Column(name = "answer_id")
     private UUID answerID;
+
+    @Column(name = "is_correct")
     private boolean isCorrect;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
 }
--- src/main/java/com/nlw/certification/modules/students/entities/CertificationStudentEntity.java
+++ src/main/java/com/nlw/certification/modules/students/entities/CertificationStudentEntity.java
@@ -1,19 +1,40 @@
 package com.nlw.certification.modules.students.entities;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity(name = "certifications")
 public class CertificationStudentEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
+
+    @JoinColumn(name = "student_id")
     private UUID studentID;
+
+    @Column(length = 100)
     private String technology;
+    @Column(length = 10)
     private int grade;
+
+    @ManyToOne
+    @JoinColumn(name = "student_id", insertable = false, updatable = false)
+    private StudentEntity studentEntity;
+
+    @OneToMany
+    @JoinColumn(name = "answer_certification_id", insertable = false, updatable = false)
     private List<AnswersCertificationEntity> answersCertificationEntities;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
 }
